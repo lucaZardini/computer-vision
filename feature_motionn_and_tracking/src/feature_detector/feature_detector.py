830777adebbf880184cf90deb814f23d7cf5f543
lucaZardini <luca.zardini@studenti.unitn.it>
2022-05-12 16:03:52 +0200

    added requirements.txt and sift detector

--- feature_motionn_and_tracking/src/feature_detector/feature_detector.py
+++ feature_motionn_and_tracking/src/feature_detector/feature_detector.py
@@ -1,16 +1,48 @@
+from __future__ import absolute_import, annotations
+
 from abc import ABC, abstractmethod
 from enum import Enum
 
+import cv2
+
 
 class FeatureDetectorAlgorithm(Enum):
-    pass
+    SIFT = "sift"
+
+
+class FeatureDetectorBuilder:
+    @staticmethod
+    def build(algorithm: FeatureDetectorAlgorithm):
+        if algorithm == FeatureDetectorAlgorithm.SIFT:
+            return SiftDetector()
+        else:
+            raise ValueError()
 
 
 class FeatureDetector(ABC):
-    def __init__(self, image):
+    def __init__(self, image=None):
         self.image = image
 
+    def with_image(self, image):
+        self.image = image
+
+    @property
+    def is_image_set(self) -> bool:
+        if self.image is None:
+            return False
+        else:
+            return True
+
     @abstractmethod
-    def detect(self, algorithm: FeatureDetectorAlgorithm):
+    def detect(self):
         pass
 
+
+class SiftDetector(FeatureDetector):
+
+    def detect(self):
+        if not self.is_image_set:
+            raise AttributeError("The image has not been set")
+        else:
+            sift = cv2.SIFT_create()
+            kp = sift.detect(self.image, None)
